344103e8fadea7fac35c9fa591f958f8c0955fc9
Leticia147 <leticia.moreira147@hotmail.com>
2021-07-24 15:23:41 -0300

    primeiro commit

---
+++ src/main/java/br/edu/utfpr/oficina/regador/RegadorApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.oficina.regador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegadorApplication.class, args);
+	}
+
+}
